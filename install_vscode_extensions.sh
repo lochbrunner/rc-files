4f9a9e511296933d7b36226eb17ca3e4da170b4b
Matthias Lochbrunner <matthias_lochbrunner@web.de>
2020-03-24 17:28:48 +0100

    Adds vscode extension list

---
+++ install_vscode_extensions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Result of:
+# code --list-extensions | xargs -L 1 echo code --install-extension
+
+code --install-extension ajshort.latex-preview
+code --install-extension betwo.vscode-linux-binary-preview
+code --install-extension bungcip.better-toml
+code --install-extension cssho.vscode-svgviewer
+code --install-extension denniskempin.vscode-include-fixer
+code --install-extension donjayamanne.githistory
+code --install-extension eamodio.gitlens
+code --install-extension James-Yu.latex-workshop
+code --install-extension letmaik.git-tree-compare
+code --install-extension lochbrunner.vscode-generic-binary-preview
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
+code --install-extension rust-lang.rust
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension tomoki1207.pdf
+code --install-extension twxs.cmake
+code --install-extension vadimcn.vscode-lldb
+code --install-extension webfreak.debug
+code --install-extension wmaurer.change-case
+code --install-extension xaver.clang-format
+code --install-extension yzhang.markdown-all-in-one
